0530d7200cdb1067375caa4b66a0ce69c8ef81da
Julianbeton <julianibeton@iskolarngbayan.pup.edu.ph>
2024-01-23 22:03:31 +0800

    uploading final code

--- printing_characters_from_a_string_that_are_present_at_an_even_index_number.py
+++ printing_characters_from_a_string_that_are_present_at_an_even_index_number.py
@@ -13,9 +13,15 @@
 
 # pseudocode
 # input word
+word = input("Enter a valid word  ")
 
 # print original string
+print("Original String:", word)
 
 # print what program run
+print(" Printing only even index characters")
 
+length_of_the_word = len(word)
 # range for word
+for i in range(0, length_of_the_word, 2):
+    print(word[i])
